970f8a1715fdf38dab347e3c56d2994f5b565c04
SilvenLEAF <SilvenLEAF@gmail.com>
2022-05-05 07:43:44 +0530

    feat(navigation): ShouXian navigation utility set up

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler'; // this MUST be on extreme top of this file (make sure there is nothing else above it)
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './NeiXin/screens/HomeScreen';
