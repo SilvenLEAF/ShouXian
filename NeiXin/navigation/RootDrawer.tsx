c46d60a727458292ba6f1bb83b361404f9bf7e54
SilvenLEAF <SilvenLEAF@gmail.com>
2022-05-05 14:31:47 +0530

    feat(navigation): ShouXian bottom tab navigation set up

--- NeiXin/navigation/RootDrawer.tsx
+++ NeiXin/navigation/RootDrawer.tsx
@@ -1,3 +1,4 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
@@ -7,13 +8,14 @@ import HomeScreen from '../screens/HomeScreen';
 
 
 // const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 function RootDrawer() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home Page" component={HomeScreen}/>
-      <Stack.Screen name="About Page" component={AboutScreen}/>
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home Page" component={HomeScreen}/>
+      <Tab.Screen name="About Page" component={AboutScreen}/>
+    </Tab.Navigator>
   )
 }
 
